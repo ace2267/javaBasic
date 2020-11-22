9157c92bb2d40cd98ef09bfa076e75fff9b90739
ace2267 <ace2267@innocore.kr>
2020-11-22 15:11:40 +0900

    java stiticblock

---
+++ javaBasicJiho/src/test/java/com/jinho/basic/JavaBasicJihoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinho.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicJihoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
